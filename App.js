12c86c62f165cd5fe9372bb043d83d71ba9deef7
Manasi Dudhane <mansi@Manasis-MBP.lan>
2023-11-21 18:30:31 -0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from 'react-redux';
+//1) Setup redux
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Heyy!</Text>
       <StatusBar style="auto" />
     </View>
   );
